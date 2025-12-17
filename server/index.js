b9a5f89d593cec60251522242aa1a1ac0db51a7c
Abhishek kumar <115467885+abhinext2244@users.noreply.github.com>
2025-12-17 09:06:41 +0530

    Update CORS origin in index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "https://auth-taupe-phi.vercel.app",
     origin:"https://auth-h3mx.vercel.app",
     // origin:"http://localhost:5173",
     credentials:true,
