1058c58d73694f12f062bcfded7ee59c2bea3754
Abhishek kumar <115467885+abhinext2244@users.noreply.github.com>
2025-12-18 19:04:11 +0530

    Enable CORS for production origin

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 /* ---------- CORS (ONLY ONE PLACE) ---------- */
 const corsOptions = {
-  // origin: "https://auth-taupe-phi.vercel.app",
+  origin: "https://auth-taupe-phi.vercel.app",
   origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
