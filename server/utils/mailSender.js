4453e93cff8d1d0ba8c6b4d73154cb7b8b28f204
Abhishek kumar <115467885+abhinext2244@users.noreply.github.com>
2025-12-18 13:43:11 +0530

    Remove API key check in mailSender
    Removed check for RESEND_API_KEY presence.

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,8 +1,6 @@
 import { Resend } from "resend";
 
-if (!process.env.RESEND_API_KEY) {
-  throw new Error("RESEND_API_KEY not loaded");
-}
+
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
